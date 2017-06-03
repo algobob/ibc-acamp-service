8069b2a54686703046d781e4679360a86a0d5e18
Anderson Luiz <algobob@gmail.com>
2017-06-03 12:27:34 -0300

    Add db test helper

---
+++ src/test/java/com/ibc/acamp/acampantecrud/helper/AcampanteRepositoryHelper.java
@@ -0,0 +1,56 @@
+package com.ibc.acamp.acampantecrud.helper;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class AcampanteRepositoryHelper {
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test.db;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+    private Sql2o sql2o;
+
+    public AcampanteRepositoryHelper(){
+        sql2o = new Sql2o(DB_CONNECTION,DB_USER, DB_PASSWORD);
+    }
+
+    public void createTables(){
+        String sql = "CREATE TABLE acampantes (" +
+                "  id int NOT NULL AUTO_INCREMENT," +
+                "  nome varchar(255)," +
+                "  sexo varchar(15)," +
+                "  idade int" +
+                ");";
+
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void insertDumbData() {
+        String sql =
+                "INSERT INTO acampantes(nome, sexo, idade) " +
+                        "VALUES ('abcd', 'feminino', 12)";
+
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void dropTables(){
+        String sql = "drop table if exists acampantes;";
+
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
