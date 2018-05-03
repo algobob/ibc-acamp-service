c1dd16ce74726af558e8e5212cc135074f2cb081
Anderson Luiz <algobob@gmail.com>
2018-05-03 15:10:13 -0300

    Rename refactor

---
+++ src/main/java/com/ibc/acamp/acampantecrud/AcampanteService.java
@@ -0,0 +1,28 @@
+package com.ibc.acamp.acampantecrud;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.List;
+
+@Singleton
+public class AcampanteService {
+
+    private final DataStoreRepository repository;
+
+    @Inject
+    public AcampanteService(DataStoreRepository repository){
+        this.repository = repository;
+    }
+
+    public boolean save(Acampante acampante) {
+        return repository.save(acampante);
+    }
+
+    public List<Acampante> fetch() {
+        return repository.fetch();
+    }
+
+    public boolean update(Acampante acampante) {
+        return repository.update(acampante);
+    }
+}
