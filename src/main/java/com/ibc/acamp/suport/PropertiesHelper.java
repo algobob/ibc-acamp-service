ed78cc27f8fbf999692cb130af0161e9dd900dd1
Anderson Luiz <algobob@gmail.com>
2018-05-11 16:07:44 -0300

    Refactor properties helper

--- src/main/java/com/ibc/acamp/suport/PropertiesHelper.java
+++ src/main/java/com/ibc/acamp/suport/PropertiesHelper.java
@@ -9,6 +9,8 @@ public final class PropertiesHelper {
 
     private static Properties appProps = new Properties();
 
+    private PropertiesHelper(){}
+
     public static void load(String env) throws IOException {
         String propertiesFilePath = String.format("/%s.properties",env);
         appProps.load(getPropertiesFileAsStream(propertiesFilePath));
