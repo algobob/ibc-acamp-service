98a7ba85db92ab806b692b5c80cf55efd8f0082f
Anderson Luiz <algobob@gmail.com>
2018-05-11 15:13:15 -0300

    Add PropertiesHelper class

--- src/main/java/com/ibc/acamp/Treino.java
+++ src/main/java/com/ibc/acamp/Treino.java
@@ -4,14 +4,18 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.ibc.acamp.acampantecrud.AcampanteController;
 import com.ibc.acamp.acampantecrud.AcampanteService;
+import com.ibc.acamp.suport.PropertiesHelper;
 import com.ibc.acamp.suport.SimpleModule;
+
+import java.io.IOException;
 
 import static spark.Spark.get;
 
 public class Treino {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Injector injector = Guice.createInjector(new SimpleModule());
         AcampanteService acampanteService = injector.getInstance(AcampanteService.class);
+        PropertiesHelper.load("test");
 
         get("/",(req,res) -> "teste!!!");
 
---
+++ src/main/java/com/ibc/acamp/suport/PropertiesHelper.java
@@ -0,0 +1,24 @@
+package com.ibc.acamp.suport;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesHelper {
+
+    private static Properties appProps = new Properties();
+
+    public static void load(String env) throws IOException {
+        String propertiesFilePath = String.format("/%s.properties",env);
+        appProps.load(getPropertiesFileAsStream(propertiesFilePath));
+    }
+
+    private static InputStream getPropertiesFileAsStream(String propertiesFilePath) throws FileNotFoundException {
+        return PropertiesHelper.class.getResourceAsStream(propertiesFilePath);
+    }
+
+    public static String getProps(String property) {
+        return appProps.getProperty(property);
+    }
+}
